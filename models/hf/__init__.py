615b7ee1493872934ccac859919ab07b66af2c62
w.vibe <muwayne+vibe@gmail.com>
2025-03-15 20:45:21 +0800

    Fix: Remove reference to non-existent WT5PreTrainedModel class

--- models/hf/__init__.py
+++ models/hf/__init__.py
@@ -1,9 +1,5 @@
-from .WT5 import *
+from .WT5.configuration import WT5Config
+from .WT5.modeling import WT5ForConditionalGeneration, WT5Model
+from .WT5.tokenization import WT5Tokenizer
 
-__all__ = [
-    'WT5ForConditionalGeneration',
-    'WT5Config',
-    'WT5PreTrainedModel',
-    'WT5Model',
-    'WT5Stack'
-]
+__all__ = ["WT5ForConditionalGeneration", "WT5Config", "WT5Model", "WT5Tokenizer"]
