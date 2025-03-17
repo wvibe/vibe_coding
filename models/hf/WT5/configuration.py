8186662104e43006a2d2b40ed856093914447611
wvibe <muwayne+vibe@gmail.com>
2025-03-17 22:13:54 +0800

    Remove unused logger import from WT5 configuration

--- models/hf/WT5/configuration.py
+++ models/hf/WT5/configuration.py
@@ -1,9 +1,6 @@
 import copy
 
 from transformers.configuration_utils import PretrainedConfig
-from transformers.utils import logging
-
-logger = logging.get_logger(__name__)
 
 
 class WT5Config(PretrainedConfig):
